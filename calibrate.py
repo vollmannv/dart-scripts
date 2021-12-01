74485ccddabdf231a77bef6207764d5a6d40a04a
leon <leon@galerie-hfling.com>
2021-12-01 13:02:50 +0100

    letsgo

--- calibrate.py
+++ calibrate.py
@@ -1,6 +1,7 @@
-import camera
+from camera import Camera
 import processing
 import json
+
 
 def calibrate(list_of_cameras):
     input("Please remove all darts from the board and press enter.")
@@ -13,6 +14,15 @@ def calibrate(list_of_cameras):
         data = {}
         data['camera' + str(camera.index)] = []
         data['camera' + str(camera.index)].append({
-            'center': x_coord
+            'center': str(x_coord)
         })
-        json.dump(data, 'camera-calibrations.json')
+        out = open("camera-calibrations.json", "w")
+        json.dump(data, out)
+        out.close()
+
+
+if __name__ == '__main__':
+    cam1 = Camera(1)
+    cam2 = Camera(2)
+    cams = [cam1, cam2]
+    calibrate(cams)
--- camera.py
+++ camera.py
@@ -5,6 +5,7 @@ import processing
 
 list_of_cameras = []
 
+
 class Camera:
 
     def __init__(self, index):
@@ -19,7 +20,8 @@ class Camera:
 
     def setControlImage(self):
         s, img = self.camera.read()
-        img = cv2.cvtColor(img, cv2.COLOR_RGBGRAY)
+        img = cv2.cvtColor(img, cv2.COLOR_RGB2GRAY)
+        self.control_image = img[340:400, 0:640]
 
     def take_picture(self):
         if self.imct < 10:
@@ -38,54 +40,50 @@ class Camera:
             else:
                 print("didnt work")
 
-    def monitor(self):
+    def monitor(self, cameras):
 
-            check, frame = self.camera.read()
+        check, frame = self.camera.read()
 
-            self.motion = 0.0
+        self.motion = 0.0
 
-            frame = frame[340:400, 0:640]
+        frame = frame[340:400, 0:640]
 
-            self.gray = cv2.cvtColor(frame, cv2.COLOR_RGB2GRAY)
+        self.gray = cv2.cvtColor(frame, cv2.COLOR_RGB2GRAY)
 
-            if self.control_image is None:
-                for (cam in list_of_cameras):
-                    cam.setControlImage()
-                return
+        if self.control_image is None:
+            for cam in cameras:
+                cam.setControlImage()
+            return
 
-            self.diff_frame = cv2.absdiff(self.control_image, self.gray)
-            self.thresh_frame = cv2.threshold(self.diff_frame, 30, 255, cv2.THRESH_BINARY)[1]
-            self.thresh_frame = cv2.dilate(self.thresh_frame, None, iterations = 2)
+        self.diff_frame = cv2.absdiff(self.control_image, self.gray)
+        self.thresh_frame = cv2.threshold(self.diff_frame, 30, 255, cv2.THRESH_BINARY)[1]
+        self.thresh_frame = cv2.dilate(self.thresh_frame, None, iterations=2)
 
-            cnts,_ = cv2.findContours(self.thresh_frame.copy(),
-                       cv2.RETR_EXTERNAL, cv2.CHAIN_APPROX_SIMPLE)
+        cnts, _ = cv2.findContours(self.thresh_frame.copy(),
+                                   cv2.RETR_EXTERNAL, cv2.CHAIN_APPROX_SIMPLE)
 
-            for contour in cnts:
-                if cv2.contourArea(contour) < 300:
-                    continue
-                self.motion = 1
-                (x, y, w, h) = cv2.boundingRect(contour)
+        for contour in cnts:
+            if cv2.contourArea(contour) < 300:
+                continue
+            self.motion = 1
+            (x, y, w, h) = cv2.boundingRect(contour)
 
-                cv2.rectangle(frame, (x, y), (x + w, y + h), (0, 255, 0), 3)
+            cv2.rectangle(frame, (x, y), (x + w, y + h), (0, 255, 0), 3)
 
-            if self.motion == 1:
-                for (cam in list_of_cameras):
-                    cam.differences()
-                    cam.take_picture()
-                    cam.setControlImage()
+        if self.motion == 1:
+            for cam in cameras:
+                cam.differences()
+                cam.take_picture()
+                print(processing.process_image(cam.diff_frame))
+                cam.setControlImage()
 
-                self.motion = 0
-                self.control_image = self.gray
-
-            cv2.imshow("Camera " + str(self.index), frame)
-
-            key = cv2.waitKey(1)
-
+            self.motion = 0
+            self.control_image = self.gray
 
     def differences(self):
         check, frame = self.camera.read()
         frame = frame[340:400, 0:640]
-        self.gray = cv2.cvtColor(frame, cv2,COLOR_RGB2GRAY)
+        self.gray = cv2.cvtColor(frame, cv2.COLOR_RGB2GRAY)
         self.diff_frame = cv2.absdiff(self.control_image, self.gray)
 
 if __name__ == '__main__':
@@ -94,4 +92,6 @@ if __name__ == '__main__':
     list_of_cameras.append(cam1)
     list_of_cameras.append(cam2)
 
-    cam1.monitor()
+    while (True):
+        cam1.monitor(list_of_cameras)
+        time.sleep(0.1)
