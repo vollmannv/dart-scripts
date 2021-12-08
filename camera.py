d0891a4b3896e0d47959c24f0ad009cbb90ca805
leon <leon@galerie-hfling.com>
2021-12-08 18:33:21 +0100

    das

--- camera.py
+++ camera.py
@@ -59,7 +59,7 @@ class Camera:
                                    cv2.RETR_EXTERNAL, cv2.CHAIN_APPROX_SIMPLE)
 
         for contour in cnts:
-            if cv2.contourArea(contour) < 300:
+            if cv2.contourArea(contour) < 400:
                 continue
             self.motion = 1
             (x, y, w, h) = cv2.boundingRect(contour)
@@ -89,13 +89,13 @@ def process_images():
     y1 = 0
     y2 = 0
     for cam in list_of_cameras:
-        if cam.index == 1:
+        if cam.index == 2:              #Links
             y1 = cam.currentval * -1
-        elif cam.index == 2:
+        elif cam.index == 1:            #Unten
             x1 = cam.currentval
-        elif cam.index == 4:
+        elif cam.index == 3:            #Oben
             x2 = cam.currentval * -1
-        else:
+        else:                           #Rechts
             y2 = cam.currentval
 
     x_coord = (x1 + x2) / 2
@@ -108,48 +108,63 @@ def process_images():
     if angle < 0:
         angle = angle + 360
     print(str("x: " + str(x_coord) + ", y: " + str(y_coord) + ", angle: " + str(angle)))
-    #print("x1: " + str(x1) + ", x2: " + str(x2) + ", y1: " + str(y1) + ", y2: " + str(y2))
+    print("x1: " + str(x1) + ", x2: " + str(x2) + ", y1: " + str(y1) + ", y2: " + str(y2))
+
+    distanceToMiddle = math.dist([x_coord, y_coord], [0, 0])
+    print(distanceToMiddle)
+    double1 = 1  #where the "double" area starts, from the middle
+    double2 = 2  #where the "double" area end, from the middle
+    tripple1 = 1  #where the "tripple" area starts
+    tripple2 = 2  #where the "tripple" area ends
+
+    def printvalue(val):
+        if double1 <= distanceToMiddle <= double2:
+            print("2 * " + str(val))
+        elif tripple1 <= distanceToMiddle <= tripple2:
+            print("3 * " + str(val))
+        else:
+            print(val)
 
     if 9 <= angle < 27:
-        print("13")
+        printvalue(13)
     elif 27 <= angle < 45:
-        print("4")
+        printvalue(4)
     elif 45 <= angle < 63:
-        print("18")
+        printvalue(18)
     elif 63 <= angle < 81:
-        print("1")
+        printvalue(1)
     elif 81 <= angle < 99:
-        print("20")
+        printvalue(20)
     elif 99 <= angle < 117:
-        print("5")
+        printvalue(5)
     elif 117 <= angle < 135:
-        print("12")
+        printvalue(12)
     elif 135 <= angle < 153:
-        print("9")
+        printvalue(9)
     elif 153 <= angle < 171:
-        print("14")
+        printvalue(14)
     elif 171 <= angle < 189:
-        print("11")
+        printvalue(11)
     elif 189 <= angle < 207:
-        print("8")
+        printvalue(8)
     elif 207 <= angle < 225:
-        print("16")
+        printvalue(16)
     elif 225 <= angle < 243:
-        print("7")
+        printvalue(7)
     elif 243 <= angle < 261:
-        print("19")
+        printvalue(19)
     elif 261 <= angle < 279:
-        print("3")
+        printvalue(3)
     elif 279 <= angle < 297:
-        print("17")
+        printvalue(17)
     elif 297 <= angle < 315:
-        print("2")
+        printvalue(2)
     elif 315 <= angle < 333:
-        print("15")
+        printvalue(15)
     elif 333 <= angle < 351:
-        print("10")
+        printvalue(10)
     else:
-        print("6")
+        print(6)
 
 
 if __name__ == '__main__':
@@ -168,5 +183,7 @@ if __name__ == '__main__':
     print("GOOOOO!")
 
     while (True):
-        cam4.monitor(list_of_cameras)
-        time.sleep(0.4)
+        cam3.monitor(list_of_cameras)
+        time.sleep(0.6)
+        cam2.monitor(list_of_cameras)
+        time.sleep(0.6)
